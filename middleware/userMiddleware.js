7f3874c5c4e2abaf9d0a20115bba78addf5b67a3
AhmadHafidz1316 <ahmadhafidzrinoputra1312@gmail.com>
2024-07-20 19:53:30 +0700

    repair dotenv

---
+++ middleware/userMiddleware.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken")
+
+exports.authMiddleware = (req,res,next) => {
+    let token
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(" ")[1]
+    }
+
+    if(!token) {
+        return next(res.status(401).json({
+            status: 401,
+            message: "Authorization Undifined"
+        }))
+    }
+
+    next()
+}
